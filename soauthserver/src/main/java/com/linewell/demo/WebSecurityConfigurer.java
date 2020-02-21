6fba52eb894d387bc19e0bef424535aec9c2995e
zdingxin <zdingxin@linewell.com>
2020-02-21 17:42:12 +0800

    add password mode auth

--- soauthserver/src/main/java/com/linewell/demo/WebSecurityConfigurer.java
+++ soauthserver/src/main/java/com/linewell/demo/WebSecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.linewell.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,4 +27,14 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //可以设置内存指定的登录的账号密码,指定角色
+        //不加.passwordEncoder(new MyPasswordEncoder())
+        //就不是以明文的方式进行匹配，会报错
+        auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+        //.passwordEncoder(new MyPasswordEncoder())。
+        //这样，页面提交时候，密码以明文的方式进行匹配。
+        auth.inMemoryAuthentication().passwordEncoder(new MyPasswordEncoder()).withUser("test-user1").password("12345").roles("ADMIN");
+    }
 }
