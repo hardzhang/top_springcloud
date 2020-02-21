3e5bf28759987a37b4d6d7e4b8676e6420f69a85
zdingxin <zdingxin@linewell.com>
2020-02-21 17:26:07 +0800

    add password mode auth

--- soauthserver/src/main/java/com/linewell/demo/WebSecurityConfigurer.java
+++ soauthserver/src/main/java/com/linewell/demo/WebSecurityConfigurer.java
@@ -3,6 +3,9 @@ package com.linewell.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -11,7 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @Date: 2020/2/21 0021 16:32
  * @Copyright: Fujian Linewell Software Co., Ltd. All rights reserved.
  */
+
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Bean
